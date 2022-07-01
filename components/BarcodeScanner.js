23d7cb2e3822f47cfe9d94c5d7b5216d5dc4e9e7
matt <matt.fletcher@bravissimo.com>
2022-07-01 17:03:14 +0100

    BarcodeScanner

---
+++ components/BarcodeScanner.js
@@ -0,0 +1,9 @@
+export default class barcodeScanner extends Component {
+  constructor(props) {
+  super(props);
+  this.handleTourch = this.handleTourch.bind(this);
+  this.state = {
+  torchOn: false
+ }
+ }
+ }
