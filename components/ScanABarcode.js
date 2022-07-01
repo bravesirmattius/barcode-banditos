b9289c986f7705b2691e5d5b781a481cad57afef
matt <matt.fletcher@bravissimo.com>
2022-07-01 11:42:21 +0100

    expo project

---
+++ components/ScanABarcode.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { View } from 'react-native-web'
+
+export default function ScanABarcode() {
+  return (
+   <View>
+    <Text>
+      Scan a Barcode below
+    </Text>
+   </View> 
+  )
+}
